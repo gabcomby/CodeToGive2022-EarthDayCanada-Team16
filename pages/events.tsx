9fd02c5b9fa5b93a805a48a761cebee212a4c420
Antoine-Lafrance <antoine.lafrance@polymtl.ca>
2022-11-07 00:32:36 -0500

    Revert "Maps in blank events"
    This reverts commit bf0951be83f319e6e02af945888eeacf4044d2db.

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import Head from 'next/head';
 import TopBar from '../components/topbar';
 import styles from '../styles/Dashboard.module.css';
@@ -210,48 +209,3 @@ export default function Events() {
 
     );
 }
-=======
-import Head from "next/head";
-import TopBar from "../components/topbar";
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-
-export default function Home() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDLTsmW-cX8AKIrbneAEMjKyPGNyPFYp2c",
-  });
-
-  const randomFarms: Array<position> = [
-    { lat: 40, lng: -80 },
-    { lat: 45, lng: -80 },
-    { lat: 41, lng: -80 },
-  ];
-
-  if (!isLoaded) return <div>Loading...</div>;
-
-  return <Map></Map>;
-}
-interface position {
-  lat: number;
-  lng: number;
-}
-
-function Map() {
-  const center = useMemo(() => ({ lat: 40, lng: -80 }), []);
-  const positions = [
-    { pos: { lat: 40, lng: -80 }, name: "ferme A" },
-    { pos: { lat: 35, lng: -83 }, name: "ferme B" },
-    { pos: { lat: 32, lng: -85 }, name: "ferme C" },
-    { pos: { lat: 37, lng: -87 }, name: "ferme D" },
-    { pos: { lat: 39, lng: -82 }, name: "ferme E" },
-  ];
-  return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      {positions.map((p) => (
-        <Marker key={p.pos.lat + p.pos.lng} position={p.pos} label={p.name} />
-      ))}
-      <Marker position={{ lat: 40, lng: -89 }} label={"Ferme F"} />
-    </GoogleMap>
-  );
-}
->>>>>>> Stashed changes
--- pages/map_screen.tsx
+++ pages/map_screen.tsx
@@ -1,41 +1,17 @@
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { useMemo } from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api'
 
 export default function Home() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDLTsmW-cX8AKIrbneAEMjKyPGNyPFYp2c",
-  });
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyDLTsmW-cX8AKIrbneAEMjKyPGNyPFYp2c',
+        
+    });
 
-  const randomFarms: Array<position> = [
-    { lat: 40, lng: -80 },
-    { lat: 45, lng: -80 },
-    { lat: 41, lng: -80 },
-  ];
+    if (!isLoaded) return <div>Loading...</div>;
 
-  if (!isLoaded) return <div>Loading...</div>;
-
-  return <Map></Map>;
-}
-interface position {
-  lat: number;
-  lng: number;
+    return (<GoogleMap zoom={10} center={{lat:44,lng:-80}} mapContainerClassName='map-container'></GoogleMap>);
 }
 
 function Map() {
-  const center = useMemo(() => ({ lat: 40, lng: -80 }), []);
-  const positions = [
-    { pos: { lat: 40, lng: -80 }, name: "ferme A" },
-    { pos: { lat: 35, lng: -83 }, name: "ferme B" },
-    { pos: { lat: 32, lng: -85 }, name: "ferme C" },
-    { pos: { lat: 37, lng: -87 }, name: "ferme D" },
-    { pos: { lat: 39, lng: -82 }, name: "ferme E" },
-  ];
-  return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      {positions.map((p) => (
-        <Marker key={p.pos.lat + p.pos.lng} position={p.pos} label={p.name} />
-      ))}
-      <Marker position={{ lat: 40, lng: -89 }} label={"Ferme F"} />
-    </GoogleMap>
-  );
+    return (<GoogleMap zoom={10} center={{lat:44,lng:-80}} mapContainerClassName='map-container'></GoogleMap>);
 }
